f4cc6e2a0354e4349c1c19f462a7acc1f366fac9
asalpert <97926434+asalpert@users.noreply.github.com>
2023-03-09 17:39:40 +0000

    expand_zip

--- mec/clean.py
+++ mec/clean.py
@@ -1,5 +1,10 @@
 import pandas as pd
 
+def expand_zip(column: pd.Series) -> pd.DataFrame:
+    df = column.str.split("-", expand=True)
+    df = df.rename(columns={0: "Zip", 1: "+4"})
+    return df
+
 
 def strip_punctuation(column: pd.Series) -> pd.Series:
     pass
