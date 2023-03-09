5e47665c15af110a84397d772e055cec36e6a11e
asalpert <97926434+asalpert@users.noreply.github.com>
2023-03-09 20:49:53 +0000

    9 tests passing

--- mec/clean.py
+++ mec/clean.py
@@ -6,11 +6,20 @@ def expand_zip(column: pd.Series) -> pd.DataFrame:
     df = df.rename(columns={0: "Zip", 1: "+4"})
     return df
 
-def standardize_element(element: str) -> str:
-    punctuation = string.punctuation
+def standardize_element(column: pd.Series) -> pd.Series:
+    punctuation = [x for x in string.punctuation]
+    standard = column
     for p in range(len(punctuation)):
-        standard = element.upper().replace(punctuation[p], "")
+        standard = standard.upper().replace(str(punctuation[p]), "")
     return standard
+
+def standardize_frame (df: pd.DataFrame) -> pd.DataFrame:
+   return df.applymap(lambda col: standardize_element(col))
+
+def street_types_to_abbr (element: str) -> str:
+    
+    pass
+
 
 
 def strip_punctuation(column: pd.Series) -> pd.Series:
